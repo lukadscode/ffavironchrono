2979c41c4ceeb38a184736718ac135a21ddf2231
LukaDaSilva <LukaDaSilva@MINI_IT13>
2025-11-26 14:49:09 +0100

    envoyer

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import AppRouter from "@/router";
 import { AuthProvider } from "@/context/AuthContext";
 import { Toaster } from "@/components/ui/toaster";
 
+// ⚠️ Note:
+// React.StrictMode can cause some third-party UI libraries (portals, dialogs, tooltips, etc.)
+// to run their mount/unmount logic twice in development, which may trigger DOM errors like
+// "Failed to execute 'removeChild' on 'Node': The node to be removed is not a child of this node."
+// To avoid these runtime errors in this app, we render without StrictMode for now.
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <AppRouter />
-      <Toaster />
-    </AuthProvider>
-  </React.StrictMode>
+  <AuthProvider>
+    <AppRouter />
+    <Toaster />
+  </AuthProvider>
 );
